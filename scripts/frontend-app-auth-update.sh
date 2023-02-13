88aa171aad0f51834cfe1b6dc98b889ea9281132
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-02-13 11:07:43 -0800

    updated script

--- scripts/frontend-app-auth-update.sh
+++ scripts/frontend-app-auth-update.sh
@@ -1,9 +1,15 @@
+# Prerequisites: jq (available in the Azure Cloud Shell)
+
+resourcegroup="diberry-app-to-app"
 frontend="diberry-frontend-2"
 backend="diberry-backend-2"
+loginParameters="scope=openid profile email offline_access api://$backend/user_impersonation"
 
-authSettings=$(az webapp auth show -g myAuthResourceGroup -n $frontend)
-authSettings=$(echo "$authSettings" | jq '.properties' | jq '.identityProviders.azureActiveDirectory.login += {"loginParameters":["scope=openid profile email offline_access api://$backend/user_impersonation"]}')
+# Get the current auth settings
+authSettings=$(az webapp auth show -g $resourcegroup -n $frontend)
 
-echo $authSettings
+# Add the login parameters to the Azure AD identity provider
+authSettings=$(echo "$authSettings" | jq '.properties' | jq '.identityProviders.azureActiveDirectory.login += {"loginParameters":[$loginParameters]}')
 
+# Update the auth settings
 az webapp auth set --resource-group myAuthResourceGroup --name $frontend --body "$authSettings"
