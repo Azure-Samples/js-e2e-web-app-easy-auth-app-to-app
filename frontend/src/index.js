689df87d363221a2aa5dc4b81b038387b8bf6012
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-02-28 12:30:12 -0800

    update for no auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+
 import { create } from './server.js';
 
 // DEFAULT port for App Service is 8080
