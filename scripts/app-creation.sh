7eb36fe4a37251ffe4bf62307f8b3c9423a34ce2
Dina Berry <diberry@microsoft.com>
2023-03-02 14:55:53 -0800

    Update app-creation.sh

--- scripts/app-creation.sh
+++ scripts/app-creation.sh
@@ -10,7 +10,7 @@ sku="Free"
 
 #cd js-e2e-web-app-easy-auth-app-to-app/
 cd ../frontend/
-az webapp up --resource-group "$groupname" --name "$frontendapp" --plan "$planname" --sku "$sku" --location "$location" --os-type "$os" --runtime "$os"
+az webapp up --resource-group "$groupname" --name "$frontendapp" --plan "$planname" --sku "$sku" --location "$location" --os-type "$os" --runtime "$nodeLts"
 cd ../backend/
 az webapp up --resource-group "$groupname" --name "$backendapp" --runtime "$nodeLts"
 az webapp config appsettings set --name "$frontendapp" --resource-group "$groupname" --settings BACKEND_URL="$backendurl"
