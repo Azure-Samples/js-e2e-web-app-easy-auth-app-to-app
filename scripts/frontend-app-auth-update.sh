75cf57d7b39e6f41cd4ba89f5d7142d88de5f802
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-03-02 12:18:25 -0800

    update both scripts

--- scripts/frontend-app-auth-update.sh
+++ scripts/frontend-app-auth-update.sh
@@ -1,9 +1,20 @@
 # Prerequisites: jq (available in the Azure Cloud Shell)
 
-# Replace <RESOURCE-GROUP-NAME> with the name of the resource group
-# Replace <FRONTEND-RESOURCE-NAME> with the name of the frontend app
-# Replace <BACKEND-RESOURCE-NAME> with the name of the backend app
+frontendapp=frontend-abc
+backendapp=backend-abc
+groupname=myAuthResourceGroup
+location="West Europe"
+os="Linux"
+planname=myPlan
+backendurl=https://$backendapp.azurewebsites.net
+nodeLts="NODE:16-lts"
+sku="FREE"
 
-authSettings=$(az webapp auth show -g <RESOURCE-GROUP-NAME> -n <FRONTEND-RESOURCE-NAME>-2)
-authSettings=$(echo "$authSettings" | jq '.properties' | jq '.identityProviders.azureActiveDirectory.login += {"loginParameters":["scope=openid profile email offline_access api://<BACKEND-RESOURCE-NAME>/user_impersonation"]}')
-az webapp auth set --resource-group <RESOURCE-GROUP-NAME> --name <FRONTEND-RESOURCE-NAME>-2 --body "$authSettings"
+authSettings=$(az webapp auth show -g "$groupname" -n "$frontendapp")
+frontendId=$(az webapp auth microsoft show --name $frontendapp --resource-group $groupname --query registration.clientId --output tsv)
+backendId=$(az webapp auth microsoft show --name "$backendapp" --resource-group $groupname --query registration.clientId --output tsv)
+
+# Must have user_impersonation scope
+authSettings=$(echo "$authSettings" | jq '.properties' | jq '.identityProviders.azureActiveDirectory.login += {"loginParameters":["scope=openid profile email offline_access api://"$backendId"/user_impersonation"]}')
+
+az webapp auth set --resource-group $groupname --name $frontendapp --body "$authSettings"
