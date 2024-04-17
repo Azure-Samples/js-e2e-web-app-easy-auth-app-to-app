fbaa45ebc491bff193533c7736291c65437f9aac
Cephas Lin <cephalin@microsoft.com>
2024-04-17 11:19:11 +0000

    Make to work with Windows

---
+++ backend/run.cjs
@@ -0,0 +1,2 @@
+// For more information on this file, see https://techcommunity.microsoft.com/t5/apps-on-azure-blog/supporting-es6-import-on-windows-app-service-node-js-iisnode/ba-p/3639037
+import("./src/index.js");
